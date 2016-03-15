6d48705d73d9e81e34bddcb1a8f4fd4058d6810f
nevenapesic96 <npnevenap@gmail.com>
2016-03-15 12:58:54 +0100

    Uvedena logicka kontrola u sve metode klase biblioteka

--- Biblioteka2016/src/biblioteka/Biblioteka.java
+++ Biblioteka2016/src/biblioteka/Biblioteka.java
@@ -10,12 +10,16 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null || knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri unosu knjige! ");
 		knjige.add(knjiga);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga==null || !knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri brisanju knjige! ");
 		knjige.remove(knjiga);
 
 	}
@@ -27,6 +31,9 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if(naslov==null)
+			throw new RuntimeException("Naslov ne sme biti null! ");
+		
 		LinkedList<Knjiga> rezultat=new LinkedList<Knjiga>();
 		for (int i = 0; i < knjige.size(); i++) {
 			if(knjige.get(i).getNaslov().contains(naslov))
